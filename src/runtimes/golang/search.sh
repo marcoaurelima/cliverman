fa6dc88b0c5192042bc728a260e78c27078a3783
Marco Aurelio Lima <marco_aurelio029@hotmail.com>
2025-12-30 15:19:33 -0300

    refact: alteração na forma de gerar url de download para Golang

--- src/runtimes/golang/search.sh
+++ src/runtimes/golang/search.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
+
+readonly url_versions="https://go.dev/dl/?mode=json&include=all"
 mapfile -t versions < <(
-  curl -fsSL "https://go.dev/dl/?mode=json&include=all" | jq -r '.[].version'
+  curl -fsSL $url_versions | jq -r '.[].version'
 )
 
 for (( i=${#versions[@]}-1; i>=0; i-- )); do
--- src/runtimes/golang/url.sh
+++ src/runtimes/golang/url.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 get_go_url() {
+  local url_base="https://go.dev/dl/go"
   local version="$1"
   local os="${2:-linux}"
   local arch="${3:-amd64}"
+  local format="tar.gz"
   
-  url="https://go.dev/dl/go${version}.${os}-$arch.tar.gz"
-  echo $url
+  echo "${url_base}${version}.${os}-$arch.${format}"
 }
 
 get_go_url $1
