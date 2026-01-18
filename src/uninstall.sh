2d0a8b099c9bb01731300a443781b312226d50b9
Marco Aurelio Lima <marco_aurelio029@hotmail.com>
2026-01-17 21:29:54 -0300

    feat: verificação de instalação para script de uninstall

--- src/uninstall.sh
+++ src/uninstall.sh
@@ -5,4 +5,23 @@ SCRIPT_DIR="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd)"
 readonly input="${1//$'\r'/}" 
 
 IFS=":" read -r name version <<< "$input"
+
+# Verificar se o runtime solicitado está instaldo no sistema
+readonly path_name="${HOME}/.cliverman/installs/${name}"
+if [[ ! -d "${path_name}" ]]; then
+  echo -e "\033[33m Runtime [${name}] não instalado ou desconhecido.\033[0m"
+  echo -e "  Nada foi alterado."
+  exit 1
+fi
+
+# Verificar se a versão do runtime solicitado está uninstalado no sistema
+readonly path_version="${HOME}/.cliverman/installs/${name}/${version}"
+if [[ ! -d "${path_version}" ]]; then
+  echo -e "\033[33m Versão [${name}:${version}] não instalado ou desconhecido.\033[0m"
+  echo -e "  Nada foi alterado."
+  exit 1
+fi
+
+echo -en "\033[0m"
+
 $SCRIPT_DIR/runtimes/${name}/uninstall.sh ${name} ${version}
