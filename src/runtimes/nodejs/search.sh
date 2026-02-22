03697be745b884455869057c6fa4c95a8bfbc9e7
Marco Aurelio Lima <marco_aurelio029@hotmail.com>
2026-02-22 10:18:39 -0300

    refact: enhance version listing in search script to include LTS information

--- src/runtimes/nodejs/search.sh
+++ src/runtimes/nodejs/search.sh
@@ -5,11 +5,17 @@ IFS=$'\n\t'
 get_all_versions() {
   readonly url_versions="https://nodejs.org/dist/index.json"
   mapfile -t versions < <(
-    curl -fsSL "${url_versions}" | jq -r '.[].version'
+    curl -fsSL "${url_versions}" | jq -r '.[] | "\(.version)\t\(.lts | if . then . else "" end)"'
   )
 
   for (( i=${#versions[@]}-1; i>=0; i-- )); do
-    echo "· ${versions[i]#v}"
+    line="${versions[i]}"
+    IFS=$'\t' read -r version lts <<< "${line}"
+    if [ -n "${lts}" ]; then
+      echo -e "· ${version#v} \033[1;92mLTS \033[0;90m(${lts})\033[0m"
+    else
+      echo "· ${version#v}"
+    fi
   done
 }
 
