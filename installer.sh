a240474a9dd34e7bddce72c78dd3c772e49d6dd0
Marco Aurelio Lima <marco_aurelio029@hotmail.com>
2026-03-08 20:22:39 -0300

    feat: create installer scipt

--- installer.sh
+++ installer.sh
@@ -9,20 +9,20 @@ cat <<'ASCII_ART'
  | |    | | \ \ / / _ \ '__| '_ ` _ \ / _` | '_ \ 
  | |____| | |\ V /  __/ |  | | | | | | (_| | | | |
   \_____|_|_| \_/ \___|_|  |_| |_| |_|\__,_|_| |_|  
-               Universal runtime manager  
 
 ASCII_ART
 
-echo "Installing Cliverman..."
-git clone https://github.com/marcoaurelima/cliverman.git "${HOME}/.cliverman2" >> /dev/null 2>&1
+echo -n "  Installing... "
+git clone https://github.com/marcoaurelima/cliverman.git "${HOME}/.cliverman" >> /dev/null 2>&1
 result=$?
 if [[ $result -ne 0 ]]; then
     echo -e "\033[0;91mError: Failed to clone Cliverman repository. Please check your internet connection and try again.\033[0m"
     exit 1
 fi
-echo -e "\033[0;92mCliverman installed successfully at ${HOME}/.cliverman\033[0m\n"
+echo -e "\033[0;92mSUCCESS\033[0m\n"
 
-echo "Please add the following line to your shell configuration file (e.g., .bashrc, .zshrc):"
-echo -e "\033[0;93mexport PATH=\"${HOME}/.cliverman/bin:${PATH}\"\033[0m"
-echo "Then, restart your terminal or run 'source ~/.bashrc' (or 'source ~/.zshrc') to start using Cliverman."       
+echo "  Instructions:"
+echo "  1. Please add the following line to your shell configuration file (e.g., .bashrc, .zshrc):"
+echo -e "\033[0;96m     export PATH=\"\${HOME}/.cliverman/bin:\${PATH}\"\033[0m"
+echo "  2. Then, restart your terminal or run 'source ~/.bashrc' (or 'source ~/.zshrc') to start using Cliverman."       
 
