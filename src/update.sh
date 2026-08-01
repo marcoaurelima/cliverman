c7f01cf21c47d0a2f351c2c18a7fcc56b5857041
Marco Aurélio Lima <marco_aurelio029@hotmail.com>
2026-08-01 20:46:13 -0300

    refact: use environment variable to define cliverman path in update script

--- src/update.sh
+++ src/update.sh
@@ -4,18 +4,18 @@ IFS=$'\n\t'
 
 # Update Cliverman to the latest version using git pull
 update_cliverman() {
-  local cliverman_dir="${HOME}/.cliverman"
-  if [[ -d "${cliverman_dir}/.git" ]]; then
+  if [[ -d "${CLIVERMAN_PATH}/.git" ]]; then
     echo "Updating Cliverman..."
     echo -en "\033[2;97m"
-    git -C "${cliverman_dir}" pull --rebase
+    git -C "${CLIVERMAN_PATH}" pull --rebase
     res=$?
     echo -en "\033[0m"
     if [[ ${res} -ne 0 ]]; then
       echo -e "Failed to update Cliverman \033[91mERROR\033[0m"
       exit 1
     fi
-    echo -e "Cliverman updated  \033[92mSUCCESS\033[0m"
+    version=$(git -C ${CLIVERMAN_PATH} describe --tags --abbrev=0)
+    echo -e "Cliverman updated (${version})  \033[92mSUCCESS\033[0m"
   else
     echo -e "Cliverman directory is not a git repository. Cannot update \033[91mERROR\033[0m"
     exit 1
