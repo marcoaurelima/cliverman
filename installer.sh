60f8e41fb44ecd74a936cecee793c1b513bc9356
Marco Aurélio Lima <marco_aurelio029@hotmail.com>
2026-08-04 08:40:34 -0300

    feat: add version tag checkout in installer script

--- installer.sh
+++ installer.sh
@@ -19,6 +19,17 @@ if [[ $result -ne 0 ]]; then
     echo -e "\033[0;91mError: Failed to clone Cliverman repository. Please check your internet connection and try again.\033[0m"
     exit 1
 fi
+
+# Switch to last version tag
+pushd "${HOME}/.cliverman" || exit 1
+git checkout "$(git describe --tags --abbrev=0)" >> /dev/null 2>&1
+result=$?
+if [[ $result -ne 0 ]]; then
+    echo -e "\033[0;91mError: Failed to checkout the latest version tag. Please check your internet connection and try again.\033[0m"
+    exit 1
+fi
+popd
+
 echo -e "\033[0;92mSUCCESS\033[0m\n"
 
 echo "  Instructions:"
