9c23bf6d7607290922c43e2c260f5c1879d46a01
Marco Aurélio Lima <marco_aurelio029@hotmail.com>
2026-09-10 11:06:10 -0300

    refact: replace `echo` with `printf`

--- installer.sh
+++ installer.sh
@@ -12,11 +12,11 @@ cat <<'ASCII_ART'
 
 ASCII_ART
 
-echo "  Installing. Wait a moment... "
+printtf "  Installing. Wait a moment...\n"
 git clone https://github.com/marcoaurelima/cliverman.git "${HOME}/.cliverman" >> /dev/null 2>&1
 result=$?
 if [[ $result -ne 0 ]]; then
-    echo -e "\033[0;91mError: Failed to clone Cliverman repository. Please check your internet connection and try again.\033[0m"
+    printf "\033[0;91mError: Failed to clone Cliverman repository. Please check your internet connection and try again.\033[0m\n"
     exit 1
 fi
 
@@ -25,15 +25,15 @@ pushd "${HOME}/.cliverman" >> /dev/null 2>&1 || exit 1
 git checkout "$(git describe --tags --abbrev=0)" >> /dev/null 2>&1
 result=$?
 if [[ $result -ne 0 ]]; then
-    echo -e "\033[0;91mError: Failed to checkout the latest version tag. Please check your internet connection and try again.\033[0m"
+    printf "\033[0;91mError: Failed to checkout the latest version tag. Please check your internet connection and try again.\033[0m\n"
     exit 1
 fi
 popd >> /dev/null 2>&1 
 
-echo -e "\033[0;92m  SUCCESS\033[0m\n"
+printf "\033[0;92m  SUCCESS\033[0m\n\n"
 
-echo "  Instructions:"
-echo "  1. Add the following line to your shell configuration (e.g., .bashrc, .zshrc):"
-echo -e "\033[0;96m     export PATH=\"\${HOME}/.cliverman/shims:\${PATH}\"\033[0m"
-echo "  2. Restart your terminal."       
+printf "  Instructions:\n"
+printf "  1. Add the following line to your shell configuration (e.g., .bashrc, .zshrc):\n"
+printf "\033[0;96m     export PATH=\"\${HOME}/.cliverman/shims:\${PATH}\"\033[0m\n"
+printf "  2. Restart your terminal.\n"       
 
